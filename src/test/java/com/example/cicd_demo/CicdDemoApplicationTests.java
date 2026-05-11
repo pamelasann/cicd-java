f1a983575916fdc9419c689086a531dfa311c2e0
Pamela Sanchez <pamela.sann@hotmail.com>
2026-05-11 13:27:33 -0600

    Test pipeline

--- src/test/java/com/example/cicd_demo/CicdDemoApplicationTests.java
+++ src/test/java/com/example/cicd_demo/CicdDemoApplicationTests.java
@@ -19,6 +19,6 @@ class CicdDemoApplicationTests {
 		ResponseEntity<String> response = restTemplate.getForEntity("/", String.class);
 
 		assertThat(response.getStatusCode().is2xxSuccessful()).isTrue();
-		assertThat(response.getBody()).isEqualTo("Hello World!");
+		assertThat(response.getBody()).isEqualTo("Hello World!!");
 	}
 }
