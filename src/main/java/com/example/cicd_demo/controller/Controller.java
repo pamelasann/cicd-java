92da233d3116d61f741547c11d0e1dbff1ff50ea
Pamela Sanchez <pamela.sann@hotmail.com>
2026-05-11 15:19:33 -0600

    Test

--- src/main/java/com/example/cicd_demo/controller/Controller.java
+++ src/main/java/com/example/cicd_demo/controller/Controller.java
@@ -9,6 +9,6 @@ import org.springframework.web.bind.annotation.RestController;
 class HelloController {
   @GetMapping
   public String hello() {
-    return "Hello World!!";
+    return "Hello World! From Java";
   }
 }
